cfd935d598c78564296e309ba44d09a0d07983f6
Shoumil Mandal <shoum217@gmail.com>
2026-07-02 18:06:58 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("Languages").addEventListener("change", function () {
+    alert("Are you sure you want to switch language? But LOL there will be no change :D");
+});
+
+// idk js till now, just for a bit fun i added from AI
