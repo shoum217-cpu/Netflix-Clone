e9c81be04c3e7d845970eb22f7d2855068751a78
Shoumil Mandal <shoum217@gmail.com>
2026-07-06 20:24:08 +0530

    Updations in the script.js and style.css

--- script.js
+++ script.js
@@ -1,7 +1,346 @@
-document.getElementById("Languages").addEventListener("change", function () {
-    alert("Are you sure you want to switch language? But LOL there will be no change :D");
-});
+const hindiHTML = `
+    <div class="main">
+        <div class="box"></div>
+        <div class="nav">
+            <div class="logo">
+                <img src="Assets/Images/logo.svg" alt="Logo">
+            </div>
 
-document.getElementById("Languages1").addEventListener("change", function () {
-    alert("Are you sure you want to switch language? But LOL there will be no change :D");
+            <div class="leftover-nav">
+                <img width="18px" height="16px" src="Assets/Images/Translate.png" alt="Language">
+                <select class="leftover-nav-select" id="Languages" name="Languages">
+                    <option value="English">English</option>
+                    <option value="Hindi" selected>हिन्दी</option>
+                </select>
+                <button class="leftover-nav-button">साइन इन करें</button>
+            </div>
+        </div>
+
+        <div class="MainText">
+            <div>अनलिमिटेड फ़िल्में, टीवी शो, और बहुत कुछ</div>
+            <div>₹149 से शुरू होता है. कभी भी कैंसल करें.</div>
+            <div>देखने के लिए तैयार हैं? अपनी मेंबरशिप बनाने या रीस्टार्ट करने के लिए अपना ईमेल एड्रेस डालें.</div>
+
+            <div class="MainText-buttons">
+                <input type="Email" placeholder="ईमेल एड्रेस">
+                <button>शुरू करें &nbsp<img
+                        src="Assets/Images/arrow_forward_ios_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png"
+                        alt="arrow"></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="line"></div>
+
+    <div class="Part1">
+        <h1>ट्रेंडिंग नाव</h1>
+
+        <div class="movies">
+            <div class="poster movie1"><span class="rank-number">1</span></div>
+            <div class="poster movie2"><span class="rank-number">2</span></div>
+            <div class="poster movie3"><span class="rank-number">3</span></div>
+            <div class="poster movie4"><span class="rank-number">4</span></div>
+            <div class="poster movie5"><span class="rank-number">5</span></div>
+            <div class="poster movie6"><span class="rank-number">6</span></div>
+            <div class="poster movie7"><span class="rank-number">7</span></div>
+            <div class="poster movie8"><span class="rank-number">8</span></div>
+            <div class="poster movie9"><span class="rank-number">9</span></div>
+            <div class="poster movie10"><span class="rank-number">10</span></div>
+        </div>
+    </div>
+
+    <div class="Part2">
+        <h1>शामिल होने की ज़्यादा वजहें</h1>
+
+        <div class="reasons">
+            <div class="poster2">
+                <h2>अपने टीवी पर आनंद लें</h2>
+                <p>स्मार्ट टीवी, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray प्लेयर के साथ ही दूसरे डिवाइस पर भी देखें.</p>
+                <img src="Assets/Images/icon01.png" alt="icon01">
+            </div>
+            <div class="poster2">
+                <h2>ऑफ़लाइन देखने के लिए अपने शो डाउनलोड करें</h2>
+                <p>अपने पसंदीदा शो और फ़िल्में सेव करें, ताकि आप कभी भी इन्हें देख सकें.</p>
+                <img src="Assets/Images/icon02.png" alt="icon02">
+            </div>
+            <div class="poster2">
+                <h2>हर जगह देखें</h2>
+                <p>बिना ज़्यादा पेमेंट किए, अपने फ़ोन, टैबलेट, लैपटॉप और टीवी पर अनलिमिटेड फ़िल्में और टीवी शो स्ट्रीम करें.</p>
+                <img src="Assets/Images/icon03.png" alt="icon03">
+            </div>
+            <div class="poster2">
+                <h2>बच्चों के लिए प्रोफ़ाइल बनाएं</h2>
+                <p>बच्चों को अपने पसंदीदा किरदारों के साथ उस रोमांचक सफ़र पर जाने दें जो सिर्फ़ उनके लिए ही है - आपकी मेंबरशिप के साथ फ़्री.</p>
+                <img src="Assets/Images/icon04.png" alt="icon04">
+            </div>
+        </div>
+
+    </div>
+
+    <div class="Part3">
+        <h1>अक्सर पूछे जाने वाले सवाल</h1>
+
+        <div class="FAQS">
+            <div class="questions">
+                <h2>Netflix क्या है?</h2> <img width="34px" height="34px"
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>Netflix की कीमत कितनी है?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>मैं कहां देख सकता/सकती हूं?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>मैं कैसे कैंसल करूं?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>मैं Netflix पर क्या देख सकता/सकती हूं?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>क्या Netflix बच्चों के लिए ठीक है?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+        </div>
+    </div>
+
+    <div class="Part4">
+
+        <h3>देखने के लिए तैयार हैं? अपनी मेंबरशिप बनाने या रीस्टार्ट करने के लिए अपना ईमेल एड्रेस डालें.</h3>
+        <div class="MainText-buttons2">
+            <input type="Email" placeholder="ईमेल एड्रेस">
+            <button>शुरू करें &nbsp<img
+                    src="Assets/Images/arrow_forward_ios_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png"
+                    alt="arrow"></button>
+        </div>
+    </div>
+
+
+    <div class="Part5">
+        <h4>कोई सवाल? <a href="tel:000-800-919-1743">000-800-919-1743</a> पर कॉल करें</h4>
+
+        <div class="Footer">
+
+            <div class="columns ">
+                <a href="https://help.netflix.com/support/412">FAQ</a>
+                <a href="http://ir.netflix.com/">इंवेस्टर संबंध</a>
+                <a href="https://help.netflix.com/legal/privacy">प्रायवेसी</a>
+                <a href="https://fast.com/">स्पीड टेस्ट</a>
+            </div>
+
+            <div class="columns ">
+                <a href="https://help.netflix.com/">सहायता केंद्र</a>
+                <a href="https://jobs.netflix.com/jobs">नौकरियां</a>
+                <a href="https://www.netflix.com/in/#">कुकी प्रेफ़रेंस</a>
+                <a href="https://help.netflix.com/legal/notices">कानूनी सूचनाएं</a>
+            </div>
+
+            <div class="columns ">
+                <a href="https://www.netflix.com/youraccount">अकाउंट</a>
+                <a href="https://www.netflix.com/watch">अकाउंट</a>
+                <a href="https://help.netflix.com/legal/corpinfo">कॉरपोरेट जानकारी</a>
+                <a href="https://www.netflix.com/in/browse/genre/839338">सिर्फ़ Netflix पर</a>
+            </div>
+
+            <div class="columns ">
+                <a href="https://media.netflix.com/">मीडिया केंद्र</a>
+                <a href="https://help.netflix.com/legal/termsofuse">उपयोग की शर्तें</a>
+                <a href="https://help.netflix.com/contactus">हमसे संपर्क करें</a>
+            </div>
+        </div>
+
+        <div class="lang-select-wrap">
+            <img width="18px" height="16px" src="Assets/Images/Translate.png" alt="Language">
+            <select class="leftover-nav-select" id="Languages1" name="Languages">
+                <option value="English">English</option>
+                <option value="Hindi" selected>हिन्दी</option>
+            </select>
+        </div>
+
+        <div class="footer-country">Netflix भारत</div>
+        <div class="footer-recaptcha">यह पक्का करने के लिए कि आप बॉट नहीं हैं, यह पेज Google reCAPTCHA द्वारा सुरक्षित है.</div>
+`
+
+const englishHTML = `
+    <div class="main">
+        <div class="box"></div>
+        <div class="nav">
+            <div class="logo">
+                <img src="Assets/Images/logo.svg" alt="Logo">
+            </div>
+
+            <div class="leftover-nav">
+                <img width="18px" height="16px" src="Assets/Images/Translate.png" alt="Language">
+                <select class="leftover-nav-select" id="Languages" name="Languages">
+                    <option value="English" selected>English</option>
+                    <option value="Hindi">हिन्दी</option>
+                </select>
+                <button class="leftover-nav-button">Sign In</button>
+            </div>
+        </div>
+
+        <div class="MainText">
+            <div>Unlimited movies, shows, and more</div>
+            <div>Starts at ₹149. Cancel at any time.</div>
+            <div>Ready to watch? Enter your email to create or restart your membership.</div>
+
+            <div class="MainText-buttons">
+                <input type="Email" placeholder="Email Address">
+                <button>Get Started &nbsp<img
+                        src="Assets/Images/arrow_forward_ios_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png"
+                        alt="arrow"></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="line"></div>
+
+    <div class="Part1">
+        <h1>Trending Now</h1>
+
+        <div class="movies">
+            <div class="poster movie1"><span class="rank-number">1</span></div>
+            <div class="poster movie2"><span class="rank-number">2</span></div>
+            <div class="poster movie3"><span class="rank-number">3</span></div>
+            <div class="poster movie4"><span class="rank-number">4</span></div>
+            <div class="poster movie5"><span class="rank-number">5</span></div>
+            <div class="poster movie6"><span class="rank-number">6</span></div>
+            <div class="poster movie7"><span class="rank-number">7</span></div>
+            <div class="poster movie8"><span class="rank-number">8</span></div>
+            <div class="poster movie9"><span class="rank-number">9</span></div>
+            <div class="poster movie10"><span class="rank-number">10</span></div>
+        </div>
+    </div>
+
+    <div class="Part2">
+        <h1>More reasons to join</h1>
+
+        <div class="reasons">
+            <div class="poster2">
+                <h2>Enjoy on your TV</h2>
+                <p>Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
+                <img src="Assets/Images/icon01.png" alt="icon01">
+            </div>
+            <div class="poster2">
+                <h2>Download your shows to watch offline</h2>
+                <p>Save your favourites easily and always have something to watch</p>
+                <img src="Assets/Images/icon02.png" alt="icon02">
+            </div>
+            <div class="poster2">
+                <h2>Watch Everywhere</h2>
+                <p>Stream Unlimited mobies and TV shows on your phone tablet laptop, and TV.</p>
+                <img src="Assets/Images/icon03.png" alt="icon03">
+            </div>
+            <div class="poster2">
+                <h2>Create profiles of kids</h2>
+                <p>Send kids on adventures with their favourite characters in a space made just for them — free with
+                    your membership.</p>
+                <img src="Assets/Images/icon04.png" alt="icon04">
+            </div>
+        </div>
+
+    </div>
+
+    <div class="Part3">
+        <h1>Frequently Asked Questions</h1>
+
+        <div class="FAQS">
+            <div class="questions">
+                <h2>What is Netflix?</h2> <img width="34px" height="34px"
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>How much does Netflix cost?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>Where can I watch?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>How do I cancel?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>What can I watch on Netflix?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+            <div class="questions">
+                <h2>Is Netflix good for kids?</h2> <img width="34px" height=""
+                    src="Assets/Images/add_2_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" alt="Plus Sign">
+            </div>
+        </div>
+    </div>
+
+    <div class="Part4">
+
+        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+        <div class="MainText-buttons2">
+            <input type="Email" placeholder="Email Address">
+            <button>Get Started &nbsp<img
+                    src="Assets/Images/arrow_forward_ios_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png"
+                    alt="arrow"></button>
+        </div>
+    </div>
+
+
+    <div class="Part5">
+        <h4>Questions? Call <a href="tel:000-800-919-1743">000-800-919-1743</a></h4>
+
+        <div class="Footer">
+
+            <div class="columns ">
+                <a href="https://help.netflix.com/support/412">FAQ</a>
+                <a href="http://ir.netflix.com/">Investor Relations</a>
+                <a href="https://help.netflix.com/legal/privacy">Privacy</a>
+                <a href="https://fast.com/">Speed Test</a>
+            </div>
+
+            <div class="columns ">
+                <a href="https://help.netflix.com/">Help Centre</a>
+                <a href="https://jobs.netflix.com/jobs">Jobs</a>
+                <a href="https://www.netflix.com/in/#">Cookie Preferences</a>
+                <a href="https://help.netflix.com/legal/notices">Legal Notices</a>
+            </div>
+
+            <div class="columns ">
+                <a href="https://www.netflix.com/youraccount">Account</a>
+                <a href="https://www.netflix.com/watch">Ways to Watch</a>
+                <a href="https://help.netflix.com/legal/corpinfo">Corporate Information</a>
+                <a href="https://www.netflix.com/in/browse/genre/839338">Only on Netflix</a>
+            </div>
+
+            <div class="columns ">
+                <a href="https://media.netflix.com/">Media Centre</a>
+                <a href="https://help.netflix.com/legal/termsofuse">Terms of Use</a>
+                <a href="https://help.netflix.com/contactus">Contact Us</a>
+            </div>
+        </div>
+
+        <div class="lang-select-wrap">
+            <img width="18px" height="16px" src="Assets/Images/Translate.png" alt="Language">
+            <select class="leftover-nav-select" id="Languages1" name="Languages">
+                <option value="English" selected>English</option>
+                <option value="Hindi">हिन्दी</option>
+            </select>
+        </div>
+
+        <div class="footer-country">Netflix India</div>
+        <div class="footer-recaptcha">This page is protected by Google reCAPTCHA to ensure you're not a bot</div>
+`
+
+document.addEventListener("change", function (e) {
+    if (e.target && (e.target.id === "Languages" || e.target.id === "Languages1")) {
+        alert("Are you sure you want to switch language?");
+
+        if (e.target.value === "Hindi") {
+            document.body.innerHTML = hindiHTML;
+        } else {
+            document.body.innerHTML = englishHTML;
+        }
+    }
 });
