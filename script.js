21af81feef77dab23658da8c6f31ef2edd65e65d
Shoumil Mandal <shoum217@gmail.com>
2026-07-06 19:20:31 +0530

    Slight Changes in js and html

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ document.getElementById("Languages").addEventListener("change", function () {
     alert("Are you sure you want to switch language? But LOL there will be no change :D");
 });
 
-// idk js till now, just for a bit fun i added from AI
+document.getElementById("Languages1").addEventListener("change", function () {
+    alert("Are you sure you want to switch language? But LOL there will be no change :D");
+});
